ab09cee2ad6ff2eac278769c45c7d03854970b25
bjallen10 <benjaminaallenjr@gmail.com>
2021-01-12 15:17:28 -0500

    remove scss and replaced with twailwind css

---
+++ myportfolio/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+
+
+
+function App() {
+  return (
+    <div className='.app'>
+     <Navbar />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ myportfolio/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <div className='flex justify-center items-center h-18 bg-black text-white footer'>
+            <p>Copyright @ 2021</p>
+        </div>
+    )
+}
+
+export default Footer
