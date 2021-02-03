ee426d677308d272d695282323474e12862fe81d
bjallen10 <benjaminaallenjr@gmail.com>
2021-02-02 22:09:32 -0500

    recreating portfolio

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,53 +1,24 @@
-import{useState,useEffect} from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import Dropdown from './components/Navbar/DropDown/Dropdown'
-import Home from './components/Pages/Home'
-import Info from './components/Pages/Info'
-import Projects from './components/Pages/Projects'
-import Skills from './components/Pages/Skills'
-import Tools from './components/Pages/Tools'
-import { Switch,Route } from 'react-router-dom'
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
-
-  useEffect(() => {
-    const hideMenu = () => {
-      if(window.innerWidth > 500 && isOpen) {
-        setIsOpen(false)
-        console.log('resize Me')
-      }
-    }
-    window.addEventListener('resize', hideMenu)
-
-    return () => {
-      window.removeEventListener('resize', hideMenu)
-    }
-  })
-
   return (
-    <>
-     <Navbar toggle={toggle}/>
-     <Dropdown isOpen={isOpen} toggle={toggle} />
-     <Switch>
-       <Route path='/' exact component={Home} />
-       <Route path='/Info' exact component={Info} />
-       <Route path='/Projects' exact component={Projects} />
-     </Switch>
-     <Header />
-     <Projects />
-     <Skills />
-     <Tools />
-     <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
